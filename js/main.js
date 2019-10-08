416146734a140a7869094568e457680c00a74769
mehdibalbali <mehdibalbali@yahoo.fr>
2019-10-08 21:06:46 +0200

    update js file

--- js/main.js
+++ js/main.js
@@ -1,20 +1,56 @@
-window.addEventListener("load", event => {
-	console.log("loaded");
-	main();
-});
 
-const main = event => {
-	
-	console.log("MAIN");
-	
-	let component = new MyComponent("dummy");
-	
+console.log("Connected !!!");
+window.onload=main;
+
+
+function dragStart(){
+    let task=document.getElementsByClassName('task');
+    for(var i=0;i< task.length;i++){
+      document.getElementsByClassName('task')[i].addEventListener('dragstart',show);
+    }
 }
 
-class MyComponent {
-	
-	constructor(name) {
-		console.log("my name is", name);
-	}
-	
+
+function show(ev){ 
+  console.log(this);
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function dropping(){
+  document.getElementsByClassName('box')[0].addEventListener('dragenter',enter);
+  document.getElementsByClassName('box')[0].addEventListener('dragover',over);
+  document.getElementsByClassName('box')[0].addEventListener('drop',drop);
+  document.getElementsByClassName('box')[1].addEventListener('dragenter',enter);
+  document.getElementsByClassName('box')[1].addEventListener('dragover',over);
+  document.getElementsByClassName('box')[1].addEventListener('drop',drop);
+}
+
+
+
+function enter(e) {
+  e.preventDefault();
+  console.log("hi");
+}
+
+function over(e){
+  e.preventDefault();
+  console.log("over");
+}
+
+function drop(ev){
+  console.log(this);
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  console.log(data);
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function DragandDrop(){
+  dragStart();
+}
+
+function main(){
+  DragandDrop();
+  dropping();
+
 }
