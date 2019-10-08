9dc54f064bf1fe72615d9adcb317968e71c3f34f
mehdibalbali <mehdibalbali@yahoo.fr>
2019-10-08 21:22:52 +0200

    update JS file

--- js/main.js
+++ js/main.js
@@ -1,56 +1,50 @@
 
 console.log("Connected !!!");
-window.onload=main;
+window.onload = main;
 
 
-function dragStart(){
-    let task=document.getElementsByClassName('task');
-    for(var i=0;i< task.length;i++){
-      document.getElementsByClassName('task')[i].addEventListener('dragstart',show);
-    }
+function dragStart() {
+  let task = document.getElementsByClassName('task');
+  for (var i = 0; i < task.length; i++) {
+    document.getElementsByClassName('task')[i].addEventListener('dragstart', show);
+  }
 }
 
 
-function show(ev){ 
-  console.log(this);
+function show(ev) {
+
   ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function dropping(){
-  document.getElementsByClassName('box')[0].addEventListener('dragenter',enter);
-  document.getElementsByClassName('box')[0].addEventListener('dragover',over);
-  document.getElementsByClassName('box')[0].addEventListener('drop',drop);
-  document.getElementsByClassName('box')[1].addEventListener('dragenter',enter);
-  document.getElementsByClassName('box')[1].addEventListener('dragover',over);
-  document.getElementsByClassName('box')[1].addEventListener('drop',drop);
+function dropping() {
+  document.getElementsByClassName('box')[0].addEventListener('dragenter', enter);
+  document.getElementsByClassName('box')[0].addEventListener('dragover', over);
+  document.getElementsByClassName('box')[0].addEventListener('drop', drop);
+  document.getElementsByClassName('box')[1].addEventListener('dragenter', enter);
+  document.getElementsByClassName('box')[1].addEventListener('dragover', over);
+  document.getElementsByClassName('box')[1].addEventListener('drop', drop);
 }
-
-
 
 function enter(e) {
   e.preventDefault();
-  console.log("hi");
+
 }
 
-function over(e){
+function over(e) {
   e.preventDefault();
-  console.log("over");
 }
 
-function drop(ev){
-  console.log(this);
+function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  console.log(data);
+  console.log(data + " is moved ");
   ev.target.appendChild(document.getElementById(data));
 }
 
-function DragandDrop(){
-  dragStart();
-}
+// fonction main 
+function main() {
 
-function main(){
-  DragandDrop();
+  dragStart();
   dropping();
 
 }
